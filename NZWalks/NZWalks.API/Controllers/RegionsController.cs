81204caba57148aa55b02bd6706e8155f42639b0
Robert Ahern <rahern@symplr.com>
2022-10-02 12:32:27 -0400

    End Of Section 3

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository _regionRepository;
+        private readonly IMapper _mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            _regionRepository = regionRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regions = await _regionRepository.GetAllAsync();
+
+            // return DTO Regions
+            //var regionsDTO = new List<Models.DTO.Region>();
+            //regions.ToList().ForEach(region =>
+            //{
+            //    var regionDTO = new Models.DTO.Region()
+            //    {
+            //        Id = region.Id,
+            //        Code = region.Code,
+            //        Name = region.Name,
+            //        Area = region.Area,
+            //        Lat = region.Lat,
+            //        Long = region.Long,
+            //        Population = region.Population
+            //    };
+            //    regionsDTO.Add(regionDTO);
+            //});
+
+            var regionsDTO = _mapper.Map<List<Models.DTO.Region>>(regions);
+
+            return Ok(regionsDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDBContext _nZWalksDBContext;
+        public RegionRepository(NZWalksDBContext nZWalksDBContext)
+        {
+            _nZWalksDBContext = nZWalksDBContext;
+        }
+
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            return await _nZWalksDBContext.Regions.ToListAsync();
+        }
+    }
+}
